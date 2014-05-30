3987cc0b6ab3038012bf1cb1c8bc3af141d2e913
Baruch Even <baruch@ev-en.org>
2014-05-30 12:59:01 +0300

    Automatically update ctags

--- ninja/ninja_helpers.py
+++ ninja/ninja_helpers.py
@@ -1,3 +1,5 @@
+import subprocess
+
 def shell_escape(str):
     """Escape str such that it's interpreted as a single argument by
 the shell."""
@@ -5,3 +7,6 @@ the shell."""
     if '"' in str:
         return "'%s'" % str.replace("'", "\\'")
     return str
+
+def cmd_exists(cmd):
+    return subprocess.call("type " + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
